d99e8c17fdaa7dc475844376b2996b6024cb8603
Peter Szabo <pts@fazekas.hu>
2020-11-06 18:20:03 +0100

    added tag removal call (setfattr -x user.lfo.tags) to the output of lfo_dump

--- locfileorg.pl
+++ locfileorg.pl
@@ -752,7 +752,11 @@ for my $fn0 (@ARGV) {
       $HC++;
       print "setfattr -n user.lfo.tags -v ".fnq($tags)." ".
         fnq(defined$printfn ? $printfn : $fn0)."\n";
-    } else { $tags=":none" }
+    } else {
+      print "setfattr -x user.lfo.tags ".
+        fnq(defined$printfn ? $printfn : $fn0)."\n";
+      $tags = ":none";
+    }
     #print "    $tags\n";
     $C++;
   }
